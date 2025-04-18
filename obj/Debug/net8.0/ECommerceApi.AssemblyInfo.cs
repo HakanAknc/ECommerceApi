50da3fbac13e2374a701c723ee7cba0ce8040731
Hakan Akinci <hakan062.2001@gmail.com>
2025-04-18 14:33:26 +0300

    library plugin

--- obj/Debug/net8.0/ECommerceApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ECommerceApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809b8ef58e7971c4f28af493bddba1fbd0440a6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
